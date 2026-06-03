1b25aea25e51f363f63aeb3b6ddab5557f1840ef
sania-fathima <saniafathima029@gmail.com>
2026-06-03 20:10:09 +0530

    Create book.java

---
+++ book.java
@@ -0,0 +1,39 @@
+public class Book {
+
+    private int bookId;
+    private String title;
+    private String author;
+    private boolean issued;
+
+    public Book(int bookId, String title, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.issued = false;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isIssued() {
+        return issued;
+    }
+
+    public void setIssued(boolean issued) {
+        this.issued = issued;
+    }
+
+    @Override
+    public String toString() {
+        return "Book ID: " + bookId +
+                " | Title: " + title +
+                " | Author: " + author +
+                " | Status: " +
+                (issued ? "Issued" : "Available");
+    }
+}
